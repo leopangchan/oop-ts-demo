f700954dd04b04a4caf753b5c7890dbb6803dde5
Yiu Pang Chan <yiu.c@leantaas.com>
2020-05-17 12:47:26 -0700

    mapping entities

--- src/app/course/course.service.ts
+++ src/app/course/course.service.ts
@@ -12,18 +12,14 @@ export class CourseService {
   constructor() {}
 
   public getCourseEntity(type: string, name: string, price: number) {
-    if (type === 'math') {
-      return new MathCourse(type, name, price);
-    } else if (type === 'eng') {
-      return new EngCourse(type, name, price);
-    } else if (type === 'hist') {
-      return new HistCourse(type, name, price);
-    } else if (type === 'java') {
-      return new JavaCourse(type, name, price);
-    } else if (type === 'node') {
-      return new NodeCourse(type, name, price);
-    } else {
-      return new Course('N/A', 'N/A', 0);
-    }
+    const courses = {
+      math: MathCourse,
+      eng: EngCourse,
+      hist: HistCourse,
+      java: JavaCourse,
+      node: NodeCourse,
+    };
+
+    return courses.hasOwnProperty(type) ? (new courses[type](type, name, price)) : new Course('N/A', 'N/A', 0);
   }
 }
